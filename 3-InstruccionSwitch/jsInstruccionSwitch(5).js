5aae4c7510c38ec429ba67d2c0e19babc90cdf3f
gabrielmaldonado164 <gabrielmaldonado164@gmail.com>
2019-07-16 18:20:12 -0300

    towebs

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,21 @@
 function mostrar()
 {
+	var laHora;
+	laHora = document.getElementById('hora').value;
+
+	switch(laHora){
+	case("7"):
+
+	case("8"):
+
+	case("9"):
+
+	case("10"):
+
+	case("11"):
+		alert("Es de mañana");
+		break
+	}
+}
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
-
 //alert (laHora);
-	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
