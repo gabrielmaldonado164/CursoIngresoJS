ec4fb7bc17bbefadb9c82ab4c768c0d829db4312
gabrielmaldonado164 <gabrielmaldonado164@gmail.com>
2019-07-31 00:51:06 -0300

    Tengo sueño

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,103 @@
 function mostrar()
 {
+    var respuesta;
+    var cantidadPares;
+    var cantidadImpares;
+    var cantidadCeros;
+    var promedioPositivos;
+    var contadorPositivo;
+    var contadorNegativo;
+    var sumaPositivos;
+    var sumaNegativos;
+    var numeroMaximo;
+    var numeroMinimo;
+    var letraMaximo;
+    var letraMIninimo;
+    var letra;
+    var numero;
+    var contador;
+
+    contadorPositivo = 0;
+    contadorNegativo = 0;
+    respuesta = 's';
+    sumaNegativos = 0;
+    sumaPositivos = 0;
+    cantidadImpares = 0;
+    cantidadCeros = 0;
+    cantidadPares = 0;
+    contador = 0;
+
+    do
+    {
+        letra = prompt("Ingrese letra");
+        numero = parseInt(prompt("Ingrese numero entre -100 y 100"));
+
+        while(isNaN(numero))
+        {
+            alert("No es un numero");
+            numero = parseInt(prompt("Ingrese numero entre -100 y 100"));
+        }
+        
+        while(!(numero>-100 && numero<100))
+        {
+            alert("Numero incorrecto");
+            numero = parseInt(prompt("Ingrese numero entre -100 y 100"));
+
+        }
+
+        if(numero%2 == 0)
+        {
+            cantidadPares++;
+        }
+        else
+        {  
+           if(numero %2==1 || numero ==-1)
+            {
+               cantidadImpares++;
+               
+            }
+
+            if(numero == 0)
+            {
+                cantidadCeros++;
+            }
+
+            if(numero >0)
+            {
+                sumaPositivos = sumaPositivos + numero;
+                contadorPositivo++;
+            }
+            else
+            {
+                sumaNegativos = sumaNegativos + numero;
+                contadorNegativo;
+            }
+            
+            if(numero == 0)
+            {
+                numeroMaximo = numero;
+                numeroMinimo = numero;
+                letraMIninimo = leta;
+                letraMaximo = letra;
+            }
+
+            if(numero>numeroMaximo)
+            {
+                numeroMaximo = numero;
+                letraMaximo = letra;
+            }
+
+            if(numero<numeroMinimo)
+            {
+                numeroMinimo = numero;
+                letraMIninimo = letra;
+            }
+
+            respuesta = prompt("Quiere continuar: s/n");
+        }
+ 
+
+    }while(respuesta == 's');
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,72 @@
 function mostrar()
-{
+{   
+    var notas; 
+    var alumnos;
+    var sexo;
+    var promedio;
+    var notaMasBaja;
+    var cantidadVarones;
+    var sumaNotas;
+    var sexoMasBajo;
+
+    alumnos = 0;
+    notaMasBaja = 0;
+    cantidadVarones = 0;
+    sumaNotas = 0;
+    sexo ='f';
+    sexo = 'm';
+
+    while(alumnos<5)
+    {
+        notas = parseInt(prompt("Ingrese una nota"));
+        while(isNaN(notas))
+        {
+            alert("Error, eso no es un numero");
+            notas = parseInt(prompt("Ingrese un numero"));
+        }
+
+        while(!(notas>0 && notas<=10))
+        {
+            alert("Eso es un numero muy grande");
+            notas = parseInt(prompt("Reingrese nota"));
+        }
+
+        sexo = prompt("Indique sexo: f/m");
+
+        while(!(sexo == 'f' || sexo == 'm'))
+        {
+            alert("Letras incorrectas");
+            sexo = prompt("Escriba nuevamente una letra");
+        }
+
+        if(alumnos == 0)
+        {
+            notaMasBaja = notas;
+            sexoMasBajo = sexo;
+        }
+        else
+        {
+            if(notas<notaMasBaja)
+            {
+                notaMasBaja = notas;
+                sexoMasBajo = sexo;
+            }
+
+            if(sexo == 'm' && notas >= 6)
+            {
+                cantidadVarones++;
+            }
+        }
+
+        alumnos++;
+
+        sumaNotas = sumaNotas + notas;
+        promedio = sumaNotas / alumnos;
+
+    }
+
+    alert("La suma de la notas es: " + sumaNotas + " y el promedio es: " + promedio);
+    alert("La nota mas baja es: " + notaMasBaja + " y el sexo es " + sexoMasBajo);
+    alert("la cantidad de varones con mayor o igual a 6 es: " + cantidadVarones);
 
 }
